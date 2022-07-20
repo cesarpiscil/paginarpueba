323a1a3af58795e1d7dc2fc430c218a8800fb058
cesarpiscil <cesarpiscil68@gmail.com>
2022-07-20 02:55:55 -0500

    ya se cargo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=p, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola profesor</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa praesentium consectetur perspiciatis explicabo non nulla tenetur ea, ducimus nobis deserunt quibusdam adipisci consequuntur ipsum sint illum natus libero, soluta voluptatum!</p>
+</body>
+</html>
